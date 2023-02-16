ff7e684d8d2fdc7d907f3a9a7a6e291e6174bbea
Lenovo <nemorashed1212@gmail.com>
2023-02-16 13:47:30 -0800

    setup public interface

---
+++ src/public interface/Home.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import About from "./About";
+import Donate from "./Donate";
+import Volunteer from "./Volunteer";
+import Article from "./Article";
+import Contact from "./Contact";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Donate />
+      <Volunteer />
+      <Article />
+      <Contact />
+    </div>
+  );
+}
+
+export default Home;
